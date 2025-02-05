efb1dd81238a50acae1aea8d3a89bf9b8458c3a3
Spywire <79043321+spywiree-priv@users.noreply.github.com>
2025-02-05 18:23:51 +0100

    refactor: rename CLI to cli

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func KillProcess(name string) ([]string, error) {
 	return nil, ErrProcessNotFound
 }
 
-var CLI struct {
+var cli struct {
 	Value string `arg:""`
 
 	SteamUserdata string `type:"existingdir" default:"${default_steam_userdata}"`
@@ -45,7 +45,7 @@ var CLI struct {
 }
 
 func main() {
-	_ = kong.Parse(&CLI,
+	_ = kong.Parse(&cli,
 		kong.Name("SteamGlobalLaunchOptions"),
 		kong.Description("A CLI tool to apply launch options for all Steam games at once."),
 		kong.UsageOnError(),
@@ -60,29 +60,29 @@ func main() {
 			"default_user_id":        GetDefaultUserId(),
 		},
 	)
-	if CLI.UserId == "" {
+	if cli.UserId == "" {
 		log.Fatalln("Default user id have not been found. You must provide user id.")
-	} else if CLI.SteamUserdata == "" {
+	} else if cli.SteamUserdata == "" {
 		log.Fatalln("Default Steam userdata folder path have not been found. You must provide Steam userdata folder path.")
 	}
 
 	args, err := KillProcess("steam")
 	if errors.Is(err, ErrProcessNotFound) {
-		CLI.DontRestartSteam = true
+		cli.DontRestartSteam = true
 	} else if err != nil {
 		log.Fatalln(err)
 	}
 
 	err = ApplyLaunchOptions(
-		CLI.Value,
-		filepath.Join(CLI.SteamUserdata, CLI.UserId, ConfigFilePath),
-		CLI.Overwrite,
+		cli.Value,
+		filepath.Join(cli.SteamUserdata, cli.UserId, ConfigFilePath),
+		cli.Overwrite,
 	)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	if !CLI.DontRestartSteam {
+	if !cli.DontRestartSteam {
 		if len(args) <= 1 {
 			_ = exec.Command("steam").Start()
 		} else {
